f4e359e78650b544c865947b87324f6be946cd32
Marcone-Alves <22401350@aluno.cotemig.com.br>
2025-11-04 08:05:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,55 @@ document.addEventListener("DOMContentLoaded", () => { //fazer ação quando rein
     }
     //o console.log serve para aparecer no f12 o que esta acontecendo
 })
+
+function modalHtml(){
+
+  let modal = document.getElementsByClassName("modal-back")[0]
+  modal.showModal()
+  document.body.style.overflow = "hidden"
+
+  if (modal.isope) {
+    
+  }
+
+}
+
+
+function modalVm(){
+
+  document.getElementsByClassName("modal-back")[1].showModal()
+  document.body.style.overflow = "hidden"
+}
+
+function modalCss(){
+
+  document.getElementsByClassName("modal-back")[2].showModal()
+  document.body.style.overflow = "hidden"
+}
+
+function modalBd(){
+
+  document.getElementsByClassName("modal-back")[3].showModal()
+  document.body.style.overflow = "hidden"
+}
+
+function modalCshar(){
+
+  document.getElementsByClassName("modal-back")[4].showModal()
+  document.body.style.overflow = "hidden"
+}
+
+function modalIng(){
+
+  document.getElementsByClassName("modal-back")[5].showModal()
+  document.body.style.overflow = "hidden"
+}
+
+function sairModal(){
+
+  for (let index = 0; index < document.getElementsByClassName("modal-back").length; index++) {
+    
+    document.getElementsByClassName("modal-back")[index].close()
+    document.body.style.overflow = "auto"
+  }
+}
